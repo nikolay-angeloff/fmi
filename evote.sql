8b852a5cb2c5322acb1df8b0e2df8a6a7a43f5e9
Nikolay Angelov <nikolay.angelov@mentormate.com>
2016-02-22 01:02:15 +0200

    sad

---
+++ evote.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 22, 2016 at 12:00 AM
+-- Server version: 5.5.36
+-- PHP Version: 5.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `evote`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `first_name`, `last_name`, `username`, `email`, `password`) VALUES
+(7, 'Niki', 'Angelov', 'admin', 'admin@admin.com', '643f29880a299c2c393244944d83d144'),
+(8, 'Николай', 'Ангелов', 'niki', 'niki@niki.com', 'n1k12010'),
+(11, 'sad', 'asd', 'asd', 'admina', 'd41d8cd98f00b204e9800998ecf8427e'),
+(12, 'Bistra', 'Angelova', 'bistra', 'bistra86@gmail.com', 'e8e663553e207ff7116e10e21bf1d3ab');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `candidate`
+--
+
+CREATE TABLE IF NOT EXISTS `candidate` (
+  `election_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  UNIQUE KEY `election_id` (`election_id`,`user_id`),
+  KEY `election_id_2` (`election_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `election`
+--
+
+CREATE TABLE IF NOT EXISTS `election` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `start_date` timestamp NULL DEFAULT '0000-00-00 00:00:00',
+  `end_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `is_active` tinyint(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `election`
+--
+
+INSERT INTO `election` (`id`, `name`, `start_date`, `end_date`, `is_active`, `created_at`) VALUES
+(2, 'Избор на студентски съвет', '2016-02-01 22:00:00', '2016-02-03 22:00:00', 0, '2016-02-18 21:10:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `group`
+--
+
+CREATE TABLE IF NOT EXISTS `group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `group`
+--
+
+INSERT INTO `group` (`id`, `title`, `created_at`) VALUES
+(1, 'Катедра Информационни Технологии', '2016-02-18 11:47:38'),
+(2, 'Катедра Алгебра', '2016-02-18 11:47:55'),
+(3, 'Администрация', '2016-02-18 11:48:18'),
+(4, 'Студентски съвет', '2016-02-18 21:07:08');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position`
+--
+
+CREATE TABLE IF NOT EXISTS `position` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `title` (`title`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `position`
+--
+
+INSERT INTO `position` (`id`, `title`, `created_at`) VALUES
+(1, 'Декан', '2016-02-18 11:36:01'),
+(2, 'Ръководител катедра', '2016-02-18 11:36:29'),
+(3, 'Професор', '2016-02-18 11:36:44'),
+(4, 'Студент', '2016-02-18 21:06:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE IF NOT EXISTS `result` (
+  `election_id` int(11) NOT NULL,
+  `candidate_id` int(11) NOT NULL,
+  `verification_code` int(11) NOT NULL,
+  `is_verified` tinyint(1) NOT NULL DEFAULT '0',
+  UNIQUE KEY `verification_code` (`verification_code`),
+  UNIQUE KEY `candidate_id_election_id` (`election_id`,`candidate_id`),
+  KEY `candidate_id` (`candidate_id`),
+  KEY `election_id` (`election_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `position` int(11) NOT NULL,
+  `group` int(11) NOT NULL,
+  `unique_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `unique_id` (`unique_id`),
+  KEY `position` (`position`),
+  KEY `group` (`group`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `position`, `group`, `unique_id`, `created_at`) VALUES
+(1, 'sad', 'asdas', 'dasd@asd.daa', 3, 2, 1455800808, '2016-02-18 13:06:48'),
+(2, 'asdas', 'asdda', 'dasd@asd.daa1', 1, 1, 1455801170, '2016-02-18 13:12:50'),
+(3, 'Тест', 'Тест', 'dasd@asd.daa12', 2, 3, 1455811636, '2016-02-18 16:07:16'),
+(4, 'Иванчо', 'Петров', 'дасдада', 4, 4, 1455829701, '2016-02-18 21:08:21');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `vote`
+--
+
+CREATE TABLE IF NOT EXISTS `vote` (
+  `election_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  UNIQUE KEY `election_id` (`election_id`,`user_id`),
+  KEY `election_id_2` (`election_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `candidate`
+--
+ALTER TABLE `candidate`
+  ADD CONSTRAINT `candidate_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `election` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `candidate_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `result`
+--
+ALTER TABLE `result`
+  ADD CONSTRAINT `result_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `election` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `result_ibfk_2` FOREIGN KEY (`candidate_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`position`) REFERENCES `position` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`group`) REFERENCES `group` (`id`);
+
+--
+-- Constraints for table `vote`
+--
+ALTER TABLE `vote`
+  ADD CONSTRAINT `vote_ibfk_1` FOREIGN KEY (`election_id`) REFERENCES `election` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
